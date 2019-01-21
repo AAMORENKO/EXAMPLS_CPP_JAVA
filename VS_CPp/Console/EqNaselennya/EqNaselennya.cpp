550071cee0d2392eea1cf63445c91c1b7db30573
ALEKSEI <42469215+AAMORENKO@users.noreply.github.com>
2019-01-21 07:08:39 +0200

    Порівняння кількості мешканців вулиці

---
+++ VS_CPp/Console/EqNaselennya/EqNaselennya.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	setlocale(LC_ALL, "rus");
+	system("color 17");
+	
+	int n, A[1000],s1,s2;
+	char z;
+	do
+	{
+		system("cls");
+		cout << "\n\n\tПорiвняння кiлькостi населення на вулицi:\n\n\t";
+
+		cout << "Введiть кiлькiсть будинкiв на вулицi N=";
+		cin >> n;
+
+		s1 = s2 = 0;
+		for (int k = 0; k < n; k++)
+		{
+			A[k] = 1 + rand() % 400;
+			printf("\tA[%i]=%i\n", k + 1, A[k]);
+			if (k % 2 != 0) s1 += A[k]; else s2 += A[k];
+		}
+
+		if (s2 >= s1)
+			cout << "\n\n\tРезультат: на парнiй сторонi проживає бiльшiсть: " << s2 << "\n\tна непарнiй сторонi: " << s1;
+		else
+			cout << "\n\n\tРезультат: на непарнiй сторонi проживає бiльшiсть: " << s1 << "\n\tна парнiй сторонi: " << s2;
+
+		cout << endl << endl << "\tПродовжити (y/n)? >> ";
+		cin >> z;
+	} while (z != 'n');
+
+	return 0;
+}
