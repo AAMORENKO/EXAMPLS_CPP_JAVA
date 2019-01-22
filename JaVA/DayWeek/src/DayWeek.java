bf62501e6370d9d04e164fa08b476effbacf006e
ALEKSEI <wzooks@gmail.com>
2019-01-22 20:02:33 +0200

    Определяем тип дня недели (исправлено)

--- JaVA/DayWeek/src/DayWeek.java
+++ JaVA/DayWeek/src/DayWeek.java
@@ -1,37 +1,59 @@
-import java.io.*;
+﻿import java.io.*;
 import java.util.*;
 
 public class DayWeek {
 
     public static void main(String[] args) {
 
-        System.out.println("Определяем тим дня недели этого года\n\n");
-        int d,m,c,y;
+        System.out.println("Определяем тип дня недели этого года\n\n");
+        int D,M,Y;
 
-        c=21;
-        y=19;
+        Y=19; // порядковый номер года, например 19 из 2019
 
         Scanner in = new Scanner(System.in);
         System.out.print("Введите день: ");
-        d=in.nextInt();
+        D=in.nextInt();
         System.out.print("Введите месяц: ");
-        m=in.nextInt();
+        M=in.nextInt();
 
-        int res=(d+(int)((13*m-1)/5)+y+(int)(y/4)+(int)(c/4)-2*c+777)%7;
-        String txt="";
 
-        switch (res)
+        int a,y,m;
+        String txt = "";
+        int res=0;
+
+        if((D>0 && D<32)&&(D!=29 && M==2))
         {
-            case 0: txt="\nЭтот день выходной. Воскресенье."; break;
-            case 1: txt="\nЭто рабочий день. Понедельник."; break;
-            case 2: txt="\nЭто рабочий день. Вторник."; break;
-            case 3: txt="\nЭто рабочий день. Среда."; break;
-            case 4: txt="\nЭто рабочий день. Четверг."; break;
-            case 5: txt="\nЭто рабочий день. Пятница."; break;
-            case 6: txt="\nЭтот день выходной. Суббота."; break;
+            a = (14 - M) / 12;
+            y = Y - a;
+            m = M + 12 * a - 2;
+            res = 7000 + (D + y + y / 4 - y / 100 + y / 400 + (31 * m) / 12);
+            res = res % 7;
 
-            default: txt="\nОшибка ввода. Такого дня нет.";
-        }
+            switch (res)
+            {
+                case 0:
+                    txt = "\nЭтот день выходной. Воскресенье.";
+                    break;
+                case 1:
+                    txt = "\nЭто рабочий день. Понедельник.";
+                    break;
+                case 2:
+                    txt = "\nЭто рабочий день. Вторник.";
+                    break;
+                case 3:
+                    txt = "\nЭто рабочий день. Среда.";
+                    break;
+                case 4:
+                    txt = "\nЭто рабочий день. Четверг.";
+                    break;
+                case 5:
+                    txt = "\nЭто рабочий день. Пятница.";
+                    break;
+                case 6:
+                    txt = "\nЭтот день выходной. Суббота.";
+                    break;
+            }
+        } else  txt = "\nОшибка ввода. Такого дня нет.";
 
         System.out.println("\t"+txt);
 
