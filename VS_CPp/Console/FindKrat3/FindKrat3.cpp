b478b27b30fbad7a7cc82d235efc089493a8f241
ALEKSEI <42469215+AAMORENKO@users.noreply.github.com>
2019-01-28 15:51:55 +0200

    Пошук елементiв матрицi номери кратнi трьом

---
+++ VS_CPp/Console/FindKrat3/FindKrat3.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	setlocale(LC_ALL, "rus");
+	system("color 1F");
+
+	int A[100];
+	int k;
+
+	cout << "\n\n\tПошук елементiв матрицi, номери яких кратнi трьом:\n\n";
+
+	
+	for (k = 0; k < 100; k++)
+	{
+		A[k] = rand();
+		if(k%3==0 && k>0) printf("A[%i]=%i\t",k, A[k]);
+	}
+
+
+	cout << endl << endl << "\t";
+	system("pause");
+	return 0;
+}
