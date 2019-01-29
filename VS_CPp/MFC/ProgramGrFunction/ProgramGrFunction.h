707ce1bc452d00f12aad6bc8faf24c4f03c4e10a
ALEKSEI <42469215+AAMORENKO@users.noreply.github.com>
2019-01-29 15:50:40 +0200

    Рисуем параболу и синусоиду

---
+++ VS_CPp/MFC/ProgramGrFunction/ProgramGrFunction.h
@@ -0,0 +1,62 @@
+﻿
+// ProgramDlg.h : header file
+//
+
+#pragma once
+
+#define mtime 298
+#define PI 3.1415926535897932384626433832795
+
+
+// CProgramDlg dialog
+class CProgramDlg : public CDialog
+{
+// Construction
+public:
+	CProgramDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_PROGRAM_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+
+	// Оголошення функцій користувача
+	afx_msg double MyF1(double q);
+	afx_msg double MyF2(double q);
+
+
+private:
+
+	COLORREF cf, c[25];
+	HFONT hold, hNew, hbk;
+	HPEN hPenOxy, hOldPen, pen;
+	HBRUSH m, oldm, brush;
+	CPen d, oldd;
+	CBitmap pic;
+	CRect rc, w, kw[30];
+
+	CString ms, t, z;
+
+	int Matrix[50][50];
+	int x1, y1, x2, y2, x3, y3, x4, y4;
+	int RH, RW, k, i, j, p, x, y, cx, cy, dx, dy;
+	bool fg;
+
+
+
+
+	DECLARE_MESSAGE_MAP()
+};
