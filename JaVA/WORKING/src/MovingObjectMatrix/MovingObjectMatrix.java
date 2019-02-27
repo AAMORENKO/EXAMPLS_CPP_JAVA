e68cbf91443e83f49102f87f9cb60e8a21d05ae3
ALEKSEI <42469215+AAMORENKO@users.noreply.github.com>
2019-02-27 16:32:40 +0200

    Повна версія - додані - Змійка Спіраль Телетайп

---
+++ JaVA/WORKING/src/MovingObjectMatrix/MovingObjectMatrix.java
@@ -0,0 +1,11 @@
+import GrMovingObject.GrMovingObject;
+
+import java.awt.*;
+
+public class MovingObjectMatrix {
+
+    public static void main(String[] args) {
+
+        new GrMovingObject();
+    }
+}
