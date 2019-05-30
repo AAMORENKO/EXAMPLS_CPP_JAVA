dc2dd00ed3cd2db4ce866a2e4c680e5dd3bd9d5e
ALEKSEI <42469215+AAMORENKO@users.noreply.github.com>
2019-05-30 07:31:00 +0300

    Програма перетворення ланцюгового дробу

---
+++ VS_CPp/Console/ChainFraction/ChainFraction.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int n, m , q, p;
+int a[25];
+fstream inf, otf;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	system("color 1F");
+	
+	cout << "\n\tПрограма перетворення ланцюгового дробу\n";
+	cout << "\n\tПочатковi данi з файлу data.txt :\n\n";
+	
+	
+	inf.open("data.txt", ios::in);
+	if (!inf) return 0;
+	
+	p = 0; a[0] = 1;
+	p++;
+	while (!inf.eof())
+	{
+		inf >> a[p];
+		printf("\t%i", a[p]);
+		p++;
+	}
+	inf.close();
+	p--;
+	printf("\tусього p=%i", p);
+
+	m =1; n = a[p];
+	while (p >1)
+	{
+		q = n;
+		p--;
+		n = a[p] * n + m;
+		m = q;
+		printf("\n\n\tm=%i; n=%i", m, n);
+	}
+
+	otf.open("res.txt", ios::out);
+	if (!otf) return 0;
+	otf << m<<" "<<n;
+	otf.flush();
+	otf.close();
+
+	cout << "\n\n\t";
+	system("pause");
+	return 0;
+}
