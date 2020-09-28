dbdf6825a18ec6366a1c0db0b86b0bafc3a1481b
ALEKSEI <42469215+AAMORENKO@users.noreply.github.com>
2020-09-28 17:40:23 +0300

    Обчислення кiлькостi iнформацiї у файлi

---
+++ VS_CPp/Working/Project_TXT_ReadFileData/Project1_MS_string/Project1_MS_string.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+fstream inf;
+int k, n;
+string ms;
+
+int main()
+{
+	setlocale(LC_ALL, ".1251");
+	system("color 1F");
+	cout << "\n\n\tТЕМА: Обчислення кiлькостi iнформацiї у файлi.\n\n";
+
+	k = n = 0;
+	inf.open("data.txt", ios::in);
+	if (!inf) return 0;
+
+	while (!inf.eof())
+	{
+		getline(inf, ms);
+		k += ms.size();
+		cout << ms << "\n";
+		n++;
+	}
+
+	inf.close();
+
+	
+	cout << "\n\n\tКiлькiсть зчитаних знакiв: " << k;
+	cout << "\n\n\tКiлькiсть строк: " << n;
+	cout << "\n\n\tКiлькiсть iнформацiї: " << (double)k / 1024 << " Кбайт";
+
+	cout << "\n\n\t";
+	system("pause");
+	return 0;
+}
